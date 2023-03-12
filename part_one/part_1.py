417ae4363511a33178db455a25744916a6b21d1b
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-03-12 13:22:19 +0300

    Add firs part python part_one

---
+++ part_one/part_1.py
@@ -0,0 +1,9 @@
+#Имя
+name = input("Enter for name: ")
+#Возраст
+age = int(input("Enter for age: "))
+#Вес
+marks = float(input("Enter for marks: "))
+print("The name is: ", name)
+print("The age is: ", age)
+print("The marks is: ", marks)
