e66ee657eb5c7748e5e4c70c8fb6a4037ffb764a
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-03-14 22:49:52 +0300

    add test seven tuturial python

---
+++ part_7/part_7.py
@@ -0,0 +1,13 @@
+enter_name_test = input("")
+print("Как Вас зовут ?")
+print(f"Привет, {enter_name_test}")
+
+
+
+b = int(input())
+print(f"{b} + {b} = ",b+b,sep="")
+print(f"{b} + {b} = ",b+b,sep="")
+print(f"{b} + {b} = ",b+b,sep="")
+print(f"{b} * {b} = ",b*b,sep="")
+print(f"{b} * {b} = ",b*b,sep="")
+print(f"{b} * {b} = ",b*b,sep="")
---
+++ parth_fyihfe/part_5.py
@@ -0,0 +1,20 @@
+print("-"*27)
+x = 3.89
+x = int(x)
+print(x)
+print("-"*27)
+s = 3.53
+f = 23.3
+s1 = float(s) + float(f)
+print(s1)
+print("-"*27)
+g1 = int(input())
+f2 = float(input())
+print("-"*27)
+x2 = 25
+y2 = 0.5
+print(x2 - y2)
+print(x2 * y2)
+print(x2 / y2)
+print(x2 ** y2)
+print("-"*27)
