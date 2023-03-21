6629f67466153ea3913ff482b608f1eb20a9d9ae
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-03-21 20:55:53 +0300

    tuturial python

---
+++ part_17/part_17.py
@@ -0,0 +1,28 @@
+# i = 0
+# i = ""
+
+# print(i, end="")
+# for i in range(2,2):
+#     print(i, end="")
+
+# for i in range(4,1,-1):
+#     print(i, end="")
+
+# a = 2
+# for i in range(3):
+#     a += i
+#     print(a)
+
+# a = 10
+# for i in range(3):
+#     a -= i
+#     print(a)
+
+# print(4, end="")
+# for i in range(1,2,-1):
+#     print(2*i, end="")
+
+a = 4
+for i in range(3):
+    a += a
+    print(a)
