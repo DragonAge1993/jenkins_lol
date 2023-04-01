c43aa71cab91d12fe82e3285bce7ef6654fa1b77
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-04-01 10:22:34 +0300

    New task for python

---
+++ uslovie_part/part_one_uslovie/minimum_to_chisla.py
@@ -0,0 +1,9 @@
+i = int(input())
+y = int(input())
+
+if i > y and y < i:
+    print(y)
+elif i == y:
+    print(i)
+elif y > i and i < y:
+    print(i)
--- uslovie_part/part_one_uslovie/part_one_uslovie.py
+++ uslovie_part/part_one_uslovie/part_one_uslovie.py
@@ -1,1 +1,46 @@
-print("hello")
+# i = int(input())
+# if i > 0:
+#     print(i)
+# else:
+#     print(-i)
+
+# i = int(input())
+# if i < 0:
+#     i = -i
+#     print(i)
+
+# i = int(input())
+# y = int(input())
+# if i > 0:
+#     if y > 0:
+#         print("Первая четверть!")
+#     else:
+#         print("Вторая четверть!")
+# else:
+#     if y > 0:
+#         print("Третья четверть!")
+#     else:
+#         print("Четвртая чертверть!")
+
+# a = int(input())
+# b = int(input())
+# c = a % 10 == 0
+# c1_float = int(c)
+# y1 = b % 10 == 0
+# y1_float = int(y1)
+# if c1_float or y1_float:
+#     print(f"Делится на 10 без остатка, результат: {c1_float} and {y1_float}")
+# else:
+#     print(f"На цело не делится, результат: {c1_float} and {y1_float}")
+
+
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:
+    print("Первая четверть!")
+elif x > 0 and y < 0:
+    print("Вторая четверть!")
+elif x < 0 and y > 0:
+    print("Третья четверть!")
+else:
+    print("Четвертая четверть!")
